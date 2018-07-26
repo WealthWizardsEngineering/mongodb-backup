2dca5da5c30d8332520f31c44926580ea1d95842
Ionut Craciunescu <ionut.craciunescu@wealthwizards.com>
2018-07-26 09:11:25 +0100

    remove unsed var, remove SSE - it's not used anymore

--- 3.6/src/restore.sh
+++ 3.6/src/restore.sh
@@ -39,15 +39,11 @@ HOST_STR=${MONGODB_HOST}
 [[ ( -n "${MONGODB_REPLICASET}" ) ]] && HOST_STR="${MONGODB_REPLICASET}/${MONGODB_HOST}"
 [[ ( -n "${MONGODB_REPLICASET}" ) ]] && REPLICA_SET_STR="replicaSet=${MONGODB_REPLICASET}&"
 
-INFLUXDB_HOSTPORT="influx.wealthwizards.io:8086"
 CMD_MKDIR="mkdir -p /tmp/${BUCKET}"
 
 MONGDB_CONNECTION_URI="mongodb://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}:${MONGODB_PORT}/admin?${REPLICA_SET_STR}authSource=admin"
 
-CMD_S3_GET="/usr/bin/s3cmd -e \
- --server-side-encryption -r \
- get s3://${S3_BACKUP_NAME} \
- /tmp/${BUCKET}"
+CMD_S3_GET="s3cmd get --recursive s3://${S3_BACKUP_NAME} /tmp/${BUCKET}"
 
 CMD_RESTORE="mongorestore \
  --host ${HOST_STR} \
